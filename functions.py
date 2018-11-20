841983cc24b6d082db1976c01db44d17c291a9ff
thirsty-penguin <40414847+thirsty-penguin@users.noreply.github.com>
2018-11-20 14:18:03 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -89,8 +89,8 @@ def main(db):
 	print ("\n***** ALPHA DIRECTORY *****")
 	print ("--- Actions ---")
 	question = input("Enter 's' for Search \nEnter 'c' to create user(s)\
-			  \nEnter 'd' to delete user(s) \nEnter 'q' to quit program \
-			  \nOption: ")
+				\nEnter 'd' to delete user(s) \nEnter 'q' to quit program \
+			  	\nOption: ")
 	if question == 'c': #Create Users
 		users_needed = int(input("\nEnter number of users that you need to create: "))
 		create_user(db, users_needed)
